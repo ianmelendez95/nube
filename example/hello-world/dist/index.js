44faa6fac627eeb2b2ed5a6d817b6f4e3823e269
Ian Melendez <imelendez@preludedynamics.com>
2022-10-17 21:26:59 -0500

    [examples] first successful call to capitalizeWords endpoint by main

--- example/hello-world/dist/index.js
+++ example/hello-world/dist/index.js
@@ -1,44 +1,84 @@
 const express = require('express')
 const fetch = require('node-fetch')
 
-const app = express()
-const port = 3000
-
 // main
 
 async function main() {
   const response = await fetch(
-    `http://localhost:3000/main`, 
-    { method: "POST", body: JSON.stringify(arguments) }
+    "http://localhost:3000/main", 
+    { 
+      method: "post", 
+      body: JSON.stringify([]),
+      headers: { 'Content-Type': 'application/json' } 
+    }
   )
 
   return await response.json()
+}
+
+function main_setup(app) {
+  app.post('/main', (req, res) => {
+    const args = req.body
+    const resultP = main_impl.apply(null, args)
+    resultP.then(result => res.send(JSON.stringify(result)))
+  })
 }
 
 async function main_impl() {
   return capitalizeWords("hello, world!")
 }
 
-function main_setup() {
-  const app = express()
-  app.post('/main', (req, res) => {
-    const args = req.body ? JSON.parse(req.body) : []
-    const resultP = main_impl.apply(null, args)
-    resultP
-      .then(result => JSON.stringify(result))
-      .then(result => res.send(result))
-  })
-  app.listen(3000)
-}
-
-main_setup()
+// capitalizeWords
 
 async function capitalizeWords(string) {
-  // const caps = await Promise.all(string.split(' ').map(capitalizeWord))
-  // console.log("Caps: ", caps)
+  const response = await fetch(
+    `http://localhost:3000/capitalizeWords`, 
+    { 
+      method: "post", 
+      body: JSON.stringify([ string ]), 
+      headers: { 'Content-Type': 'application/json' } 
+    }
+  )
+
+  return await response.json()
+}
+
+function capitalizeWords_setup(app) {
+  app.post('/capitalizeWords', (req, res) => {
+    const args = req.body
+    const resultP = capitalizeWords_impl.apply(null, args)
+    resultP.then(result => { res.send(JSON.stringify(result)) })
+  })
+}
+
+async function capitalizeWords_impl(string) {
   return (await Promise.all(string.split(' ').map(capitalizeWord))).join(' ')
 }
+
+// capitalizeWord
 
 async function capitalizeWord(string) {
   return string[0].toUpperCase() + string.slice(1)
 }
+
+// runtime setup
+
+const _setup_functions = [
+  main_setup,
+  capitalizeWords_setup
+]
+
+const app = express()
+
+app.use(express.json())
+
+for (let setup_function of _setup_functions) {
+  setup_function(app)
+}
+
+app.listen(3000, async () => {
+  const result = await main() 
+  if (typeof result != 'undefined') {
+    console.log(result)
+  }
+})
