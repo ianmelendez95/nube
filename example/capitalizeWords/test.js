e5cd68e642abd5e6c9fa1f7cd07d8714b1b505ad
Ian Melendez <imelendez@preludedynamics.com>
2022-10-29 12:08:48 -0500

    [examples] updated capitalizeWords test

--- example/capitalizeWords/test.js
+++ example/capitalizeWords/test.js
@@ -1,4 +1,4 @@
-const { handler } = require('./capitalizeWords.js')
+const { handler } = require('./dist/capitalizeWords.js')
 
 handler({ body: JSON.stringify(["hello there world"]) })
   .then(x => console.log("Result: ", x))
