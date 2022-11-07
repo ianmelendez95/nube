b118995cce8f43257e38b04c00d4f9dc43943f34
Ian Melendez <imelendez@preludedynamics.com>
2022-11-06 20:40:42 -0600

    [gen] added api gateway resource gen

--- src/Gen/CF.hs
+++ src/Gen/CF.hs
@@ -4,40 +4,94 @@
 module Gen.CF where 
 
 import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
 
 import Data.Aeson
 import Data.Aeson.Key
-import Data.Aeson.Encode.Pretty
+import qualified Data.Aeson.Encode.Pretty as JSONPretty
 import Data.String (fromString)
+import Data.Char
 
+import Data.Text.Encoding (decodeUtf8)
 import qualified Data.ByteString.Lazy.Char8 as BS
 
-data Param = Param {
-  paramName    :: T.Text,
+import Data.Map (Map)
+import qualified Data.Map as Map
+
+import qualified JS.Syntax as S
+
+data Named a = Named {
+  namedName :: T.Text,
+  namedItem :: a
+}
+
+newtype Param = Param {
   paramDefault :: T.Text
 }
 
-mkCfTemplate :: [Param] -> Value
-mkCfTemplate params = object [paramsKV params]
+newtype RApi = RApi {
+  apiName :: T.Text
+}
 
-paramsKV :: [Param] -> (Key, Value)
-paramsKV params = 
-  "Parameters" .= object (map paramKV params)
+instance ToJSON Param where 
+  toJSON (Param def) = object [ "Type" .= fromText "String", "Default" .= def ]
 
-paramKV :: Param -> (Key, Value)
-paramKV (Param name def) =
-  let body :: Value
-      body = object [ "Type" .= fromText "String", "Default" .= def ]
-   in fromText name .= body
+instance ToJSON RApi where 
+  toJSON (RApi name) =
+    object [ "Type" .= fromText "AWS::ApiGatewayV2::Api" 
+           , "Properties" .= object 
+               [ "Name" .= name
+               , "ProtocolType" .= fromText "HTTP"
+               ]
+           ]
 
-test_params :: [Param]
-test_params = 
-  [ Param "CapitalizeWordsBucket" "capitalizewords-bucket"
-  , Param "CapitalizeWordsRole"   "arn:aws:iam::565652071321:role/service-role/hello-aws-role-aoys6qk7"
+templateFromScript :: S.Script -> T.Text
+templateFromScript script = 
+  let name = S.scriptName script
+      api  = apiFromScriptName name
+   in encodePretty (mkCfTemplate test_params api)
+
+mkCfTemplate :: [Named Param] -> Named RApi -> Value
+mkCfTemplate params api = object 
+  [ "Parameters" .= object (map namedKV params)
+  , "Resources" .= object 
+      [ namedKV api ]
   ]
 
-test_gen :: IO ()
-test_gen = do 
-  let temp = mkCfTemplate test_params
-  BS.putStrLn (encodePretty temp)
+-- API
+
+apiFromScriptName :: T.Text -> Named RApi
+apiFromScriptName name = Named (capitalizeFirst name <> "Api") (RApi (name <> "-api")) 
+
+-- Named
+
+namedKV :: ToJSON a => Named a -> (Key, Value)
+namedKV (Named name value) = (fromText name, toJSON value)
+
+-- Text 
+
+encodePretty :: ToJSON a => a -> T.Text
+encodePretty = decodeUtf8 . BS.toStrict . JSONPretty.encodePretty
+
+capitalizeFirst :: T.Text -> T.Text
+capitalizeFirst txt = 
+  case T.uncons txt of 
+    Nothing -> txt
+    Just (c, cs) -> toUpper c `T.cons` cs
+
+-- Test Misc.
+
+test_params :: [Named Param]
+test_params = 
+  [ Named "CapitalizeWordsBucket" $ Param "capitalizewords-bucket"
+  , Named "CapitalizeWordsRole"   $ Param "arn:aws:iam::565652071321:role/service-role/hello-aws-role-aoys6qk7"
+  ]
+
+test_api_res :: Named RApi
+test_api_res = Named "CapitalizeWordsApi" (RApi "capitalizeWords-api")
+
+-- test_gen :: IO ()
+-- test_gen = do 
+--   let temp = mkCfTemplate test_params
+--   TIO.putStrLn (encodePretty temp)
  
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+
 module Lib where
 
 import qualified Data.Text as T
@@ -6,13 +9,15 @@ import qualified Data.Text.IO as TIO
 import qualified Gen.Lambda as GL
 import qualified JS.Parse as P
 import qualified JS.Syntax as S
+import qualified Gen.CF as CF
 
 import qualified Data.Aeson as J
 import qualified Data.ByteString.Lazy as BS
 
 import System.FilePath
+import qualified Gen.CF as CF
 
-js_file = "example/capitalizeWords/src.js"
+js_file = "example/capitalizeWords/capitalizeWords.js"
 
 yaml_file = "example/capitalizeWords/dist-bck/aws-stack.yaml"
 
@@ -29,6 +34,15 @@ compileFile = do
       scripts = GL.jsFunsToScripts js
       deploy_script = GL.jsFunsToDeployScript js
   GL.writeScripts (takeDirectory js_file </> "dist") deploy_script scripts
+
+  let cf_txt = CF.templateFromScript script
+  TIO.writeFile (templateFilePath $ S.scriptName script) cf_txt
+  where 
+    templateFilePath script_name = 
+      dist_dir </> T.unpack (script_name <> "-template.json")
+
+    dist_dir = takeDirectory js_file </> "dist"
+
 
 -- convertYaml :: IO ()
 -- convertYaml = do 
