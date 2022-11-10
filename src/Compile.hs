c47b4bd0f9277ca9b698ee4a1af1e52a063e9be8
Ian Melendez <imelendez@preludedynamics.com>
2022-11-09 20:15:12 -0600

    [compile] added file name check

--- src/Compile.hs
+++ src/Compile.hs
@@ -1,10 +1,10 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-
-module Lib where
+module Compile where
 
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
+import Data.Char
 
 import qualified Gen.Lambda as GL
 import qualified JS.Parse as P
@@ -23,6 +23,8 @@ import Util.Aeson
 
 compileFile :: FilePath -> IO ()
 compileFile js_file = do
+  assertValidJsFileName js_file
+
   script <- P.parseJsFile js_file
   let js = S.scriptFuns script
       scripts = GL.jsFunsToScripts js
@@ -37,3 +39,13 @@ compileFile js_file = do
 
     dist_dir = takeDirectory js_file </> "dist"
 
+
+assertValidJsFileName :: FilePath -> IO ()
+assertValidJsFileName file_path = 
+  let ext = takeExtension file_path
+      basename = takeBaseName file_path
+   in if ext /= ".js"
+        then error $ "Expecting .js file extension - has: " <> ext
+        else if not (all isLetter basename)
+               then error $ "Only letters allowed in file basename: " <> basename
+               else pure ()
