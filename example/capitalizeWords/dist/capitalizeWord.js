c5b6567b4a5e4c500e49875f51fd607b6088834a
Ian Melendez <imelendez@preludedynamics.com>
2022-11-10 21:40:15 -0600

    [example] update capitalizeWords example

--- example/capitalizeWords/dist/capitalizeWord.js
+++ example/capitalizeWords/dist/capitalizeWord.js
@@ -1,11 +1,20 @@
-const https = require('https')
-const { Buffer } = require('node:buffer')
+const {
+  capitalizeWords
+} = require('proxies')
 
 exports.handler = async function(event) {
   try {
+    let args = (typeof event.body === 'undefined' || event.body.trim().length === 0) 
+      ? [] 
+      : JSON.parse(event.body) 
+
+    if (!Array.isArray(args)) {
+      args = [args]
+    }
+
     return {
       statusCode: 200,
-      body: JSON.stringify(await capitalizeWord.apply(null, JSON.parse(event.body)))
+      body: JSON.stringify(await capitalizeWord.apply(null, args), null, 2)
     }
   } catch (e) {
     console.log(e)
@@ -16,46 +25,3 @@ exports.handler = async function(event) {
 async function capitalizeWord(word)  {
   return word[0].toUpperCase() + word.slice(1)
 }
-
-async function capitalizeWords(string)  {
-  const argsString = JSON.stringify(
-    Array.from(arguments).slice(0, capitalizeWords.length))
-
-  const options = {
-    hostname: process.env.AWS_GATEWAY_HOST,
-    port: 443,
-    path: "/capitalizeWords",
-    method: 'POST'
-  };
-
-  return new Promise((resolve, reject) => {
-    const req = https.request(options, (res) => {
-      let data = ''
-
-      res.on('data', (chunk) => {
-        data += chunk
-      })
-
-      res.on('end', () => {
-        resolve(JSON.parse(data))
-      })
-
-      res.on('error', (e) => {
-        console.error('Response Error: ', e)
-        reject(e)
-      })
-    })
-
-    req.setHeader('Content-Type', 'application/json')
-    req.setHeader('Content-Length', Buffer.byteLength(argsString))
-
-    req.on('error', (e) => {
-      console.error('Request Error: ', e)
-      reject(e)
-    })
-
-    req.end(argsString)
-  })
-}
-
-
--- example/capitalizeWords/dist/capitalizeWords.js
+++ example/capitalizeWords/dist/capitalizeWords.js
@@ -1,11 +1,20 @@
-const https = require('https')
-const { Buffer } = require('node:buffer')
+const {
+  capitalizeWord
+} = require('proxies')
 
 exports.handler = async function(event) {
   try {
+    let args = (typeof event.body === 'undefined' || event.body.trim().length === 0) 
+      ? [] 
+      : JSON.parse(event.body) 
+
+    if (!Array.isArray(args)) {
+      args = [args]
+    }
+
     return {
       statusCode: 200,
-      body: JSON.stringify(await capitalizeWords.apply(null, JSON.parse(event.body)))
+      body: JSON.stringify(await capitalizeWords.apply(null, args), null, 2)
     }
   } catch (e) {
     console.log(e)
@@ -17,46 +26,3 @@ async function capitalizeWords(string)  {
   const words = await Promise.all(string.split(' ').map(capitalizeWord))
   return words.join(' ')
 }
-
-async function capitalizeWord(word)  {
-  const argsString = JSON.stringify(
-    Array.from(arguments).slice(0, capitalizeWord.length))
-
-  const options = {
-    hostname: process.env.AWS_GATEWAY_HOST,
-    port: 443,
-    path: "/capitalizeWord",
-    method: 'POST'
-  };
-
-  return new Promise((resolve, reject) => {
-    const req = https.request(options, (res) => {
-      let data = ''
-
-      res.on('data', (chunk) => {
-        data += chunk
-      })
-
-      res.on('end', () => {
-        resolve(JSON.parse(data))
-      })
-
-      res.on('error', (e) => {
-        console.error('Response Error: ', e)
-        reject(e)
-      })
-    })
-
-    req.setHeader('Content-Type', 'application/json')
-    req.setHeader('Content-Length', Buffer.byteLength(argsString))
-
-    req.on('error', (e) => {
-      console.error('Request Error: ', e)
-      reject(e)
-    })
-
-    req.end(argsString)
-  })
-}
-
-
