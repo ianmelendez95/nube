1f206bf5328452e8117573176c2fa671e105c351
Ian Melendez <imelendez@preludeedc.com>
2025-11-01 16:20:47 -0500

    remove all stack project files

--- example/capitalizeWords/capitalizeWords.js
+++
@@ -1,8 +0,0 @@
-async function capitalizeWords(string) {
-  const words = await Promise.all(string.split(' ').map(capitalizeWord))
-  return words.join(' ')
-}
-
-async function capitalizeWord(word) {
-  return word[0].toUpperCase() + word.slice(1)
-}
