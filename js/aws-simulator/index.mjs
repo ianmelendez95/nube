36678b9f1a5ed1a082f1cc6f47bb4cd83d600f96
Ian Melendez <imelendez@preludeedc.com>
2025-11-09 17:11:05 -0600

    working setup

--- js/aws-simulator/index.mjs
+++ js/aws-simulator/index.mjs
@@ -1,6 +1,5 @@
 
 async function main() {
-    console.log("TRACE: ", process.env.NODE_PATH);
     process.env["SQS_BASE_URL"] = 'sqs:';
 
     const {handler} = await import('./src/capitalizeWords/capitalizeWord/index.mjs')
