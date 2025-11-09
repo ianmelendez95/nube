d48188f635b9065026be526dfb4fc32f84a2b712
Ian Melendez <imelendez@preludeedc.com>
2025-11-09 16:13:27 -0600

    save wip

--- js/aws-simulator/src/sqs/sqs-client.ts
+++ js/aws-simulator/src/sqs/sqs-client.ts
@@ -1,15 +1,31 @@
 export class SQSClient {
+    send(command: any) {
+        console.log("SQSClient send called with command:", command);
+    }
+}
+
+type SQSCommandInput = {[key: string]: any};
+
+class SQSCommand {
+    _command: SQSCommandInput;
+
+    constructor(command: SQSCommandInput) {
+        this._command = command;
+    }
+
+    toString() {
+        return JSON.stringify(this._command);
+    }
+}
+
+export class SendMessageCommand extends SQSCommand {
 
 }
 
-export class SendMessageCommand {
-
+export class DeleteMessageCommand extends SQSCommand {
 }
 
-export class DeleteMessageCommand {
-}
-
-export class ReceiveMessageCommand {
+export class ReceiveMessageCommand extends SQSCommand {
 
 }
 
--- js/aws-simulator/test/example/capitalizeWords/capitalizeWord_test.ts
+++ js/aws-simulator/test/example/capitalizeWords/capitalizeWord_test.ts
@@ -1,10 +1,18 @@
-import {handler} from './capitalizeWord/index.mjs';
+import { v5 } from '@std/uuid';
 
-Deno.test("capitalizeWord invoke directly", () => {
+Deno.test("capitalizeWord invoke directly", async () => {
+    Deno.env.set("SQS_BASE_URL", "sqs:");
+
+    const {handler} = await import('./capitalizeWord/index.mjs')
     handler({
         Records: [
             {
-                body: JSON.stringify(["hello"])
+                body: JSON.stringify(["hello"]),
+                messageAttributes: {
+                    OriginRequestId: {
+
+                    }
+                }
             },
             {
                 body: JSON.stringify(["world"])
