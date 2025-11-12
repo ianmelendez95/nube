5c5d671f6c4bacc8f6bce52f43349034be25f2dc
Ian Melendez <imelendez@preludeedc.com>
2025-11-12 09:22:29 -0600

    test suite modules

---
+++ test/Test.hs
@@ -0,0 +1,16 @@
+module Main where
+
+import Test.Hspec (hspec, describe, it, shouldBe, shouldThrow, anyException)
+import Control.Exception (evaluate)
+import Test.JS.Parse (jsParseSpec)
+
+main :: IO ()
+main = hspec $ do
+    -- jsParseSpec
+
+    describe "Prelude.head" $ do
+        it "returns the first element of a list" $ do
+            head [23 ..] `shouldBe` (23 :: Int)
+
+        it "throws an exception if used with an empty list" $ do
+            evaluate (head []) `shouldThrow` anyException
---
+++ test/Test/JS/Parse.hs
@@ -0,0 +1,6 @@
+module Test.JS.Parse (
+    jsParseSpec
+) where
+
+jsParseSpec :: IO ()
+jsParseSpec = undefined
