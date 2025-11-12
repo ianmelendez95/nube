0111637f028430fd8317b0aac913be8c5983d51a
Ian Melendez <imelendez@preludeedc.com>
2025-11-12 09:13:24 -0600

    first hspec

--- Test.hs
+++ Test.hs
@@ -1,4 +1,13 @@
 module Main where
 
+import Test.Hspec
+import Control.Exception (evaluate)
+
 main :: IO ()
-main = putStrLn "PASS"
+main = hspec $ do
+  describe "Prelude.head" $ do
+    it "returns the first element of a list" $ do
+      head [23 ..] `shouldBe` (23 :: Int)
+
+    it "throws an exception if used with an empty list" $ do
+      evaluate (head []) `shouldThrow` anyException
