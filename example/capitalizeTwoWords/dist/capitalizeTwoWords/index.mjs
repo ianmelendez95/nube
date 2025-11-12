c7273e2b4c967c55750d6ac914fa0338b31b1145
Ian Melendez <imelendez@preludeedc.com>
2025-11-11 21:00:20 -0600

    await on actions

--- example/capitalizeTwoWords/dist/capitalizeTwoWords/index.mjs
+++ example/capitalizeTwoWords/dist/capitalizeTwoWords/index.mjs
@@ -10,5 +10,5 @@ async function capitalizeTwoWords(_ctx)  {
   _ctx.frame.words = string.split(' ');
   _ctx.frame.word1 = _ctx.frame.words[0];
   _ctx.frame.word2 = _ctx.frame.words[1];
-  _ctx.callCC('capitalizeWord', [_ctx.frame.word1],'capitalizeTwoWords1');
+  await _ctx.callCC('capitalizeWord', [_ctx.frame.word1],'capitalizeTwoWords1');
 }
