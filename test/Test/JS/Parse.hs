fafa930b6aab0193fa6a0e3331036052f281c5bc
Ian Melendez <imelendez@preludeedc.com>
2025-11-12 13:09:19 -0600

    identifier test

--- test/Test/JS/Parse.hs
+++ test/Test/JS/Parse.hs
@@ -3,9 +3,36 @@ module Test.JS.Parse
   )
 where
 
+import Data.Text
+  ( Text,
+    pack
+  )
+import JS.Parse
+  ( Parser (..),
+    identifier,
+  )
 import Test.Hspec
   ( SpecWith (..),
+    describe,
+    it,
+    shouldBe,
+  )
+import Text.Megaparsec
+  ( errorBundlePretty,
+    runParser,
   )
 
 jsParseSpec :: SpecWith ()
-jsParseSpec = undefined
+jsParseSpec = testIdentifier
+
+testIdentifier :: SpecWith ()
+testIdentifier =
+  describe "identifier" $ do
+    it "returns the identifier" $ do
+      id <- testParser identifier (pack "hello")
+      id `shouldBe` (pack "hello")
+
+testParser :: Parser a -> Text -> IO a
+testParser parser content = do
+  let result = either (error . errorBundlePretty) id $ runParser parser "test.js" content
+  pure result
