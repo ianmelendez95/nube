b61dec0b28b8d8acb83951b871baed6e6bbe1b86
Ian Melendez <imelendez@preludeedc.com>
2025-11-18 21:19:47 -0600

    runCompilerT initial impl

--- app/Nube/Compiler.hs
+++ app/Nube/Compiler.hs
@@ -5,7 +5,7 @@ module Nube.Compiler
   )
 where
 
-import Control.Monad.Except (ExceptT, runExcept)
+import Control.Monad.Except (ExceptT, runExcept, runExceptT)
 import Control.Monad.Identity (Identity)
 import Control.Monad.Reader (ReaderT, runReaderT)
 import Nube.Context (NContext (..))
@@ -18,3 +18,6 @@ type CompilerT m a = ReaderT NContext (ExceptT String m) a
 
 runCompiler :: NContext -> Compiler a -> Either String a
 runCompiler context transpiler = runExcept $ runReaderT transpiler context
+
+runCompilerT :: (Monad m) => NContext -> CompilerT m a -> m (Either String a)
+runCompilerT context compiler_t = runExceptT $ runReaderT compiler_t context
