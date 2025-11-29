f4f1aa482fbb6f564959282d68b5bef7fa463791
Ian Melendez <ian.melendez.95@gmail.com>
2025-11-29 16:48:56 -0600

    test parseFile util

--- test/Test/Files.hs
+++ test/Test/Files.hs
@@ -6,9 +6,17 @@ import Data.Text
 import Data.Text.IO
 import System.FilePath
 import System.Directory
+import Nube.Parser (Parser)
+import Test.Util.Parse (testParser)
 
 readTestFile :: FilePath -> IO Text
 readTestFile rel_path =
   if isAbsolute rel_path 
     then readFile rel_path
     else readFile $ "test-files" </> rel_path
+
+parseTestFile :: FilePath -> Parser a -> IO a
+parseTestFile rel_path p = do 
+  content <- readTestFile rel_path
+  testParser p content
+  
